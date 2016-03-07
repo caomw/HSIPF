15abd0b531c7948dcc21723696c1d33c7b088d02
linbaowei <lin-bao-wei@hotmail.com>
2016-03-07 09:06:40 +0800

    HSIPF class

---
+++ build/CMakeFiles/hsipf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hsipf.dir/HSIPF.cpp.o"
+  "CMakeFiles/hsipf.dir/main.cpp.o"
+  "hsipf.pdb"
+  "hsipf"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/hsipf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
